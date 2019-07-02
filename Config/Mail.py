a9bb7619d2fe226458d6afa77ae2cea8667773ef
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2019-07-02 17:43:33 -0300

    Adicionado Arquivos de Configuração

---
+++ Config/Mail.py
@@ -0,0 +1,6 @@
+mailuser = 'Endereço do Email'
+mailpass = 'Senha do Email'
+smtpserver = 'smtp.softin.com.br'
+smtpport = 25
+fromaddr = 'Email de'
+toaddrs = 'para'
---
+++ Config/SqlServer.py
@@ -0,0 +1,11 @@
+
+databases = {
+    'Nome do Banco de Dados':{
+        'empresa':'Softin Sistemas',
+        'subject':'Assunto dos Emails'
+    }
+}
+backup_base_dir = "diretorio de backup"
+dbserver = 'localhost'
+u = 'user'
+p = 'password'
