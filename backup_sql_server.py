b71512cfb493367899f29a26ba2c9e52713fed4a
Gabriel Meyer <ghmeyer0@gmail.com>
2019-08-28 10:29:22 -0300

    Adicionado suporte a backup paralelos com threading

--- backup_sql_server.py
+++ backup_sql_server.py
@@ -1,59 +1,55 @@
-import utilities.system as system
 import sys
-import utilities.notification as mail
+import subprocess
+import os
+import threading
+from datetime import datetime
+
+import utilities.system as system
+import utilities.notification as notifications
 # import utilities.aws as aws
 import configuration.sql_server as sql_config
-import subprocess
 
-from datetime import datetime
-import os
-
-
-
-
-def backup_mmsql():
+def backup_mmsql(database):
     now = datetime.now()
-    for database in sql_config.DATABASES:
-        try:
-            backup_directory = sql_config.BASE_DIRECTORY +'/'+ database +'/'+ str(now.year) +'/'+ str(now.month) +'/'+ str(now.day) +'/'
-            backup_filename = database+'-' + str(now.hour) + str(now.strftime("%M")) + '.bak'
-            # aws_s3_object =  database +'/'+ str(now.year) +'/'+ str(now.month) +'/'+ str(now.day) +'/' + backup_filename.replace('.bak','.zip')
-            
-            system.create_folder(
-                backup_directory
-            )
-            create_sql_file(
-                database, 
-                backup_directory, 
-                backup_filename
-            )
-            run_backup(
-                backup_directory, 
-                backup_filename
-            )
-            system.compact_file(
-                backup_directory, 
-                backup_filename
-            )
-            # aws.upload_file_to_s3(
-            #     backup_directory, 
-            #     backup_filename.replace('.bak','.zip'), 
-            #     sql_config.AWS_BUCKET, 
-            #     aws_s3_object
-            # )
-            end_time = datetime.now() - now
-            mail.send_message(
-                "Backup da base "+ database, 
-                "Backup da base " + database + " no servidor " + sql_config.DATABASE_SERVER + " foi realizado." + 
-                "\n Duração: " + str(end_time), 
-                '#03fc1c'
-            )
-        except Exception as e:
-            mail.send_logs(
-                "Falha no Backup! Favor Verificar!", 
-                backup_directory + "log.txt", 
-                "Error: %s" % e
-            )
+    try:
+        backup_directory = sql_config.BASE_DIRECTORY +'/'+ database +'/'+ str(now.year) +'/'+ str(now.month) +'/'+ str(now.day) +'/'
+        backup_filename = database+'-' + str(now.hour) + str(now.strftime("%M")) + '.bak'
+        # aws_s3_object =  database +'/'+ str(now.year) +'/'+ str(now.month) +'/'+ str(now.day) +'/' + backup_filename.replace('.bak','.zip')
+        system.create_folder(
+            backup_directory
+        )
+        create_sql_file(
+            database, 
+            backup_directory, 
+            backup_filename
+        )
+        run_backup(
+            backup_directory, 
+            backup_filename
+        )
+        system.compact_file(
+            backup_directory, 
+            backup_filename
+        )
+        # aws.upload_file_to_s3(
+        #     backup_directory, 
+        #     backup_filename.replace('.bak','.zip'), 
+        #     sql_config.AWS_BUCKET, 
+        #     aws_s3_object
+        # )
+        end_time = datetime.now() - now
+        notifications.send_message(
+            "Backup da base "+ database, 
+            "Backup da base " + database + " no servidor " + sql_config.DATABASE_SERVER + " foi realizado." + 
+            "\n Duração: " + str(end_time), 
+            '#03fc1c'
+        )
+    except Exception as e:
+        notifications.send_logs(
+            "Falha no Backup! Favor Verificar!", 
+            backup_directory + "log.txt", 
+            "Error: %s" % e
+        )
 
 def create_sql_file(database_name, backup_directory, backup_filename):
     file = open(
@@ -79,7 +75,24 @@ def run_backup(backup_directory, backup_filename):
         system.write_log_file("End backup: " + backup_directory + backup_filename, backup_directory)
     else:
         system.write_log_file("Backup failed: " + backup_directory + backup_filename, backup_directory)
-        mail.send_logs("Falha no Backup! Favor Verificar!", backup_directory + "backup.log")
+        notifications.send_logs("Falha no Backup! Favor Verificar!", backup_directory + "backup.log")
         exit()
 
-backup_mmsql()
+
+backups = []
+try:
+    for database in sql_config.DATABASES:
+        backups.append(threading.Thread(target=backup_mmsql, args=(database,)))
+except Exception as e:
+        notifications.send_logs(
+            "Falha no Backup! Favor Verificar!", 
+            0, 
+            "Error: %s" % e
+        )
+
+    
+for backup in backups:
+    backup.start()
+    
+for backup in backups:
+    backup.join()
